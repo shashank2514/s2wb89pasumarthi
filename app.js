e26e3ff3499db57a7e38a1c7364e95aaf1b9348c
shashankpasumarthi <98116079+shashank2514@users.noreply.github.com>
2022-04-06 20:29:31 -0500

    Adding new files to main branch

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var watchRouter= require('./routes/watch');
+var addmodsRouter= require('./routes/addmods');
+var selctorRouter= require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/watch', watchRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selctorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
